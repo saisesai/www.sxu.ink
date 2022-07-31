fab3a61dfb9e53aafbd1aaadf150eb95e228eccd
郭晶辉 <ther@therPC.local>
2022-07-31 18:42:40 +0800

    Add jwt

--- go.mod
+++ go.mod
@@ -3,16 +3,23 @@ module github.com/saisesai/www.sxu.ink
 go 1.17
 
 require (
-	github.com/antonfisher/nested-logrus-formatter v1.3.1 // indirect
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/gin-gonic/gin v1.8.1
+	github.com/golang-jwt/jwt/v4 v4.4.2
+	github.com/google/uuid v1.3.0
+	github.com/qiniu/qmgo v1.1.0
+	github.com/sirupsen/logrus v1.8.1
+	go.mongodb.org/mongo-driver v1.9.0
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -21,14 +28,11 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/qiniu/qmgo v1.1.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+
 	"github.com/gin-gonic/gin"
 	"github.com/saisesai/www.sxu.ink/config"
 	"github.com/saisesai/www.sxu.ink/controller"
-	"github.com/saisesai/www.sxu.ink/middleware/gin-logrus"
+	"github.com/saisesai/www.sxu.ink/middleware/auth"
+	gin_logrus "github.com/saisesai/www.sxu.ink/middleware/gin-logrus"
 	"github.com/sirupsen/logrus"
 )
 
@@ -27,20 +29,31 @@ func main() {
 
 	router.Use(gin_logrus.Logger(logrus.StandardLogger()), gin.Recovery())
 
-	router.POST("/api/artwork/new", controller.ArtworkAddHandler)
-	router.POST("/api/artwork/del/:uuid", controller.ArtworkDeleteHandler)
-	router.POST("/api/artwork/get/:uuid", controller.ArtworkGetHandler)
-	router.POST("/api/artwork/set/:uuid", controller.ArtworkSetHandler)
+	router.POST("/api/user/register", controller.UserRegisterHandler)
+	router.POST("/api/user/login", controller.UserLoginHandler)
+	jwtAuth := router.Group("/", auth.JWTAuth())
+	{
+		jwtAuth.POST("/api/hello", controller.Hello)
 
-	router.POST("/api/image/new", controller.ImageAddHandler)
-	router.POST("/api/image/del/:uuid", controller.ImageDeleteHandler)
-	router.POST("/api/image/get/:uuid", controller.ImageGetHandler)
-	router.POST("/api/image/set/:uuid", controller.ImageSetHandler)
+		jwtAuth.POST("/api/artwork/new", controller.ArtworkAddHandler)
+		jwtAuth.POST("/api/artwork/del/:uuid", controller.ArtworkDeleteHandler)
+		jwtAuth.POST("/api/artwork/get/:uuid", controller.ArtworkGetHandler)
+		jwtAuth.POST("/api/artwork/set/:uuid", controller.ArtworkSetHandler)
 
-	router.POST("/api/char/new", controller.CharAddHandler)
-	router.POST("/api/char/del/:uuid", controller.CharDeleteHandler)
-	router.POST("/api/char/get/:uuid", controller.CharGetHandler)
-	router.POST("/api/char/set/:uuid", controller.CharSetHandler)
+		jwtAuth.POST("/api/image/new", controller.ImageAddHandler)
+		jwtAuth.POST("/api/image/del/:uuid", controller.ImageDeleteHandler)
+		jwtAuth.POST("/api/image/get/:uuid", controller.ImageGetHandler)
+		jwtAuth.POST("/api/image/set/:uuid", controller.ImageSetHandler)
+
+		jwtAuth.POST("/api/char/new", controller.CharAddHandler)
+		jwtAuth.POST("/api/char/del/:uuid", controller.CharDeleteHandler)
+		jwtAuth.POST("/api/char/get/:uuid", controller.CharGetHandler)
+		jwtAuth.POST("/api/char/set/:uuid", controller.CharSetHandler)
+
+		jwtAuth.POST("/api/user/setpassword", controller.UserSetPassword)
+		jwtAuth.POST("/api/user/setnickname", controller.UserSetNickName)
+		jwtAuth.POST("/api/user/setgroup", controller.UserSetGroup)
+	}
 
 	err = router.Run(*config.Listen)
 	if err != nil {
